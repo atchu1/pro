0ce2ac3c7b1f17799a06040936e21a52428e624a
atchu1 <atchayaraj18@gmail.com>
2019-06-07 11:25:13 +0530

    Create pali.py

---
+++ pali.py
@@ -0,0 +1,12 @@
+M=input()
+A=""
+p=0
+li=[]
+if M==M[::-1]:
+li.append(0)
+for i in range(0,len(M)-1):
+for j in range(i+1,len(M)):
+A=M[i:j+1]
+if A==A[::-1]:
+li.append(len(M)-len(A))
+print(min(li))
