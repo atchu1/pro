950184ac9dbba2b481137de9f5abfa60158e2420
atchu1 <atchayaraj18@gmail.com>
2019-06-07 11:58:05 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,8 @@
+a,m=map(int,input().split())
+p,s=map(int,input().split())
+b,r=map(int,input().split())
+v,h=map(int,input().split())
+if(a+p+b+v==m+s+r+h):
+    print("yes")
+else:
+    print("no")
